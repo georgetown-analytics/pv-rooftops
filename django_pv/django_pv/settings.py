f019a2378fe9e27c4f35528515dbd3d602b6a6b6
jameelahmyoung <jyoung@live.com>
2017-06-19 15:07:08 -0400

    Update settings.py

--- django_pv/django_pv/settings.py
+++ django_pv/django_pv/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'django_pv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['pv/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
